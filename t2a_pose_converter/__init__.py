2b43510ba7315de2b2566aee214c3c8b4bb04086
Cathut <necota2496@gmail.com>
2025-04-19 23:57:53 +0900

    修正

--- t2a_pose_converter/__init__.py
+++ t2a_pose_converter/__init__.py
@@ -6,7 +6,7 @@
 bl_info = {
     "name": "T2A PoseConverter",
     "author": "CatHut",
-    "version": (1, 1, 0),
+    "version": (1, 0, 1),
     "blender": (3, 6, 0),
     "location": "View3D > Tool Shelf > CatHut",
     "description": "Convert pose between T‑pose and A‑pose",
